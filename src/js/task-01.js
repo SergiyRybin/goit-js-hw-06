7d9312d1dc7bfd50836f84f3d2287d208782f41a
SergiyRybin <serhiobax@gmail.com>
2022-01-16 15:49:04 +0200

    all

---
+++ src/js/task-01.js
@@ -0,0 +1,19 @@
+const AllCategories = document.querySelectorAll('.item')
+console.log(`Number of categories: ${AllCategories.length} ` )
+
+
+const animals = AllCategories[0]
+const animalLength = animals.querySelectorAll('li')
+console.log(`Category: ${animals.firstElementChild.textContent}`)
+console.log(`Elements: ${animalLength.length}`)
+
+
+const products = AllCategories[1]
+const productsLength = products.querySelectorAll('li')
+console.log(`Category: ${products.firstElementChild.textContent}`)
+console.log(`Elements: ${productsLength.length}`)
+
+const technologies = AllCategories[2]
+const technologiesLength = technologies.querySelectorAll('li')
+console.log(`Category: ${technologies.firstElementChild.textContent}`)
+console.log(`Elements: ${technologiesLength.length}`)
