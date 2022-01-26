cc7c8aa41604047b3391779f2d51f9d4f48aa996
SergiyRybin <serhiobax@gmail.com>
2022-01-26 21:17:13 +0200

    1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,18 +2,25 @@ const AllCategories = document.querySelectorAll('#categories .item')
 console.log(`Number of categories: ${AllCategories.length} ` )
 
 
-const animals = AllCategories[0]
-const animalLength = animals.querySelectorAll('li')
-console.log(`Category: ${animals.firstElementChild.textContent}`)
-console.log(`Elements: ${animalLength.length}`)
+// const animals = AllCategories[0]
+// const animalLength = animals.querySelectorAll('li')
+// console.log(`Category: ${animals.firstElementChild.textContent}`)
+// console.log(`Elements: ${animalLength.length}`)
 
 
-const products = AllCategories[1]
-const productsLength = products.querySelectorAll('li')
-console.log(`Category: ${products.firstElementChild.textContent}`)
-console.log(`Elements: ${productsLength.length}`)
+// const products = AllCategories[1]
+// const productsLength = products.querySelectorAll('li')
+// console.log(`Category: ${products.firstElementChild.textContent}`)
+// console.log(`Elements: ${productsLength.length}`)
 
-const technologies = AllCategories[2]
-const technologiesLength = technologies.querySelectorAll('li')
-console.log(`Category: ${technologies.firstElementChild.textContent}`)
-console.log(`Elements: ${technologiesLength.length}`)
+// const technologies = AllCategories[2]
+// const technologiesLength = technologies.querySelectorAll('li')
+// console.log(`Category: ${technologies.firstElementChild.textContent}`)
+// console.log(`Elements: ${technologiesLength.length}`)
+
+AllCategories.forEach( function(categori){
+    console.log(`Category: ${categori.firstElementChild.textContent}`)
+console.log(`Elements: ${categori.lastElementChild.children.length}`)
+
+})
+
--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,8 +1,10 @@
 const input = document.querySelector("#validation-input")
-console.log(Number(input.dataset.length))
+
 input.addEventListener("blur", (event)=>{
     console.log(event.target.value.length)
    if(event.target.value.length === Number(input.dataset.length)){
-       console.log("ok")
-   }else{ console.log("lost")}
+       input.setAttribute("id", "#validation-input.valid")
+    }else{ 
+   input.setAttribute("id", "validation-input.invalid")
+}
 })
