484a59017eaecf38ad73aa1ba9829bf0e85b1567
SergiyRybin <serhiobax@gmail.com>
2022-01-27 19:02:13 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,7 +1,6 @@
 const input = document.querySelector("#validation-input")
 
 input.addEventListener("blur", (event)=>{
-    console.log(event.target.value.length)
    if(event.currentTarget.value.length >= event.currentTarget.dataset.length){
        input.classList.add('valid')
        input.classList.remove('invalid')
