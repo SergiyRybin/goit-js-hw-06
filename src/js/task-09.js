77b83cd140de55ff397848cfabe9f4e939660d77
SergiyRybin <serhiobax@gmail.com>
2022-01-29 11:35:57 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanColor = document.querySelector(".color")
+const buttonChange = document.querySelector(".change-color")
+buttonChange.addEventListener("click", () => {
+  const currentCollor = getRandomHexColor()
+  document.body.style.backgroundColor = currentCollor
+  spanColor.textContent = currentCollor
+}) ;
+
+
+ 
+
+
+
