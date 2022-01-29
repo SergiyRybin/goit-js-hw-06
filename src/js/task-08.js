fb99a1c0adf821960147ebd43ee7ac71c1e7cd56
SergiyRybin <serhiobax@gmail.com>
2022-01-29 11:08:54 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector(".login-form")
+form.addEventListener("submit", (event)=>{
+event.preventDefault();
 
+const{elements: {email, password}}= event.currentTarget;
+if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const data = {};
+  data.name = email.value
+  data.password = password.value
+})
