df3921f6616913626ad40a41099f5134da074255
SergiyRybin <serhiobax@gmail.com>
2022-01-29 12:59:04 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonCreate = document.querySelector("[data-create]")
+const buttonDestroy = document.querySelector("[data-destroy]")
+const divBox = document.querySelector("#boxes")
+const addBox = document.createElement('div')
+
+buttonDestroy.addEventListener("click", ()=> {
+  console.log(buttonDestroy.textContent)
+  addBox.remove()
+})
+
+buttonCreate.addEventListener("click", ()=> {
+  console.log(buttonCreate.textContent)
+  const addBox = document.createElement('div')
+
+divBox.append(addBox)
+})
+
