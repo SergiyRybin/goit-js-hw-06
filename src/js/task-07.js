5d7afcb6c7c3a7f7ce7f455d14536b2afebe99b3
SergiyRybin <serhiobax@gmail.com>
2022-02-04 20:02:24 +0200

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,9 +1,7 @@
 const input = document.querySelector('#font-size-control')
 const spanText = document.querySelector("#text")
-
-
+spanText.style.fontSize = input.value+"px"
 const change = () =>  spanText.style.fontSize = input.value+"px"
-    // spanText.style.fontSize = `${input.value}px`
 ;
 
 input.addEventListener("change", change);
